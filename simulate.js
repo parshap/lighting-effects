dd37ba23900602bb4711991da2f502e9423b6f4e
Parsha Pourkhomami <parshap+git@gmail.com>
2015-06-23 03:26:48 -0700

    Refactor simulator usage

--- simulate.js
+++ simulate.js
@@ -6,12 +6,13 @@ var createSimulator = require("opc-simulator");
 var createOPCStream = require("opc");
 var createStrand = require("opc/strand");
 
-var simulator = createSimulator(STRAND_LENGTH);
-var stream = createOPCStream();
-stream.pipe(simulator);
+var effect = require("./effects/weather");
 
-var knightrider = require("./effects/knight-rider");
 var strand = createStrand(STRAND_LENGTH);
-knightrider(strand).on("data", function() {
-  stream.writePixels(0, strand.buffer);
-});
+createSimulator(function() {
+  var stream = createOPCStream();
+  effect(strand).on("data", function() {
+    stream.writePixels(0, strand.buffer);
+  });
+  return stream;
+}).listen(process.env.PORT || 8080);
